519bd79e8721518b191bfbdc1e0bd94e60393d10
Lakshay Saini <93211383+WhitehatLaksh@users.noreply.github.com>
2022-10-10 22:31:21 +0530

    Initial commit

---
+++ fastapi-master/app/main.py
@@ -0,0 +1,50 @@
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from starlette.responses import Response
+
+from app.db.models import UserAnswer
+from app.api import api
+import pytest
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Fast API in Python"}
+
+
+@app.get("/user")
+def read_user():
+    return api.read_user()
+
+
+@app.get("/question/{position}", status_code=200)
+def read_questions(position: int, response: Response):
+    question = api.read_questions(position)
+
+    if not question:
+        raise HTTPException(status_code=400, detail="Error")
+
+    return question
+
+
+@app.get("/alternatives/{question_id}")
+def read_alternatives(question_id: int):
+    return api.read_alternatives(question_id)
+
+
+@app.post("/answer", status_code=201)
+def create_answer(payload: UserAnswer):
+    payload = payload.dict()
+
+    return api.create_answer(payload)
+
+
+@app.get("/result/{user_id}")
+def read_result(user_id: int):
+    return api.read_result(user_id)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, workers=2, debug=True)
+
